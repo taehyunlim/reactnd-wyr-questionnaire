72c851a32da871dc1960848b4f247cc24d752fa6
taehyunlim <taehyun.lim@gmail.com>
2018-11-13 21:00:06 -0800

    Re-route to Login if authedUser not set

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import Question from './Question'
 
 class QuestionPage extends Component {
+  
   render() {
+    const { authedUser, id } = this.props
+    
+    // Re-route to Login page if authedUser not set
+    if (!authedUser) {
+      return <Redirect to={'/login'}  />
+    }
+
     return (
       <div className='page-container'>
-        <Question id={this.props.id} isPollView={true} />
+        <Question id={id} isPollView={true} />
       </div>
     )
   }
