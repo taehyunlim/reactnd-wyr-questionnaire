d9e32badd71e177a7fdf124f2dd4fdcb1b46afea
taehyunlim <taehyun.lim@gmail.com>
2018-12-02 12:42:26 -0800

    Updated Nav to show profile page for chosen user

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,77 @@
-import React, { Component, Fragment } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { handleInitialData } from '../actions'
-import LoadingBar from 'react-redux-loading'
-import './App.css';
-import { Nav } from './Nav'
-import Questions from './Questions'
-import QuestionPage from './QuestionPage'
-import NewQuestion from './NewQuestion'
-import Login from './Login'
-import LeaderBoard from './LeaderBoard'
-import QuestionsByUser from './QuestionsByUser'
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions";
+import LoadingBar from "react-redux-loading";
+import "./App.css";
+import { Nav } from "./Nav";
+import Questions from "./Questions";
+import QuestionPage from "./QuestionPage";
+import NewQuestion from "./NewQuestion";
+import Login from "./Login";
+import LeaderBoard from "./LeaderBoard";
+import ProfilePage from "./ProfilePage";
 
 class App extends Component {
   // App level state for UI
-  state = { 
+  state = {
     activeTab: 0,
     activeTabSort: 0
-  }
+  };
   componentDidMount() {
-    this.props.dispatch(handleInitialData())
+    this.props.dispatch(handleInitialData());
   }
   // Passing down as a prop using render method: https://tylermcginnis.com/react-router-pass-props-to-components/
-  selectTab = index => this.setState({ activeTab: index })
-  selectTabSort = index => this.setState( {activeTabSort: index })
+  selectTab = index => this.setState({ activeTab: index });
+  selectTabSort = index => this.setState({ activeTabSort: index });
 
   render() {
-    const { authedUser, loading } = this.props
+    const { authedUser, loading } = this.props;
 
     return (
       <Router>
         <Fragment>
           <LoadingBar />
           <div className="container">
-            <Nav />
-            {!authedUser && 
-              <Login />
-            }
-            {!(loading) &&
+            <Nav authedUser={authedUser} />
+            {!authedUser && <Login />}
+            {!loading && (
               <div>
-                <Route path='/' exact render={() => <Questions activeTab={this.state.activeTab} selectTab={this.selectTab} />} />
-                <Route path='/question/:id' component={QuestionPage} />
-                <Route path='/new' component={NewQuestion} />
-                <Route path='/leader-board' exact render={() => <LeaderBoard activeTabSort={this.state.activeTabSort} selectTabSort={this.selectTabSort} />} />
-                <Route path='/profile' exact render={() => <QuestionsByUser activeTab={this.state.activeTab} selectTab={this.selectTab} user={authedUser} />} />
+                <Route
+                  path="/"
+                  exact
+                  render={() => (
+                    <Questions
+                      activeTab={this.state.activeTab}
+                      selectTab={this.selectTab}
+                    />
+                  )}
+                />
+                <Route path="/question/:id" component={QuestionPage} />
+                <Route path="/new" component={NewQuestion} />
+                <Route
+                  path="/leader-board"
+                  exact
+                  render={() => (
+                    <LeaderBoard
+                      activeTabSort={this.state.activeTabSort}
+                      selectTabSort={this.selectTabSort}
+                    />
+                  )}
+                />
+                <Route
+                  path="/profile/:user"
+                  exact
+                  render={props => (
+                    <ProfilePage
+                      activeTab={this.state.activeTab}
+                      selectTab={this.selectTab}
+                      {...props}
+                    />
+                  )}
+                />
               </div>
-            }
+            )}
           </div>
         </Fragment>
       </Router>
@@ -53,12 +79,12 @@ class App extends Component {
   }
 }
 
-function mapStateToProps ({ authedUser, users }) {
+function mapStateToProps({ authedUser, users }) {
   return {
     loading: users === null,
     authedUser
-  }
+  };
 }
 
 // Using the connect() function upgrades a component to a container. Containers can read state from the store and dispatch actions.
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,43 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import ProfileWidget from './ProfileWidget'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import ProfileWidget from "./ProfileWidget";
 
-export const Nav = () => {
+export const Nav = props => {
   return (
-    <div className='nav-container'>
-      <nav className='nav'>
+    <div className="nav-container">
+      <nav className="nav">
         <ul>
           <li>
-            <NavLink to='/' exact activeClassName='active'>
+            <NavLink to="/" exact activeClassName="active">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to='/new' activeClassName='active'>
+            <NavLink to="/new" activeClassName="active">
               New Question
             </NavLink>
           </li>
           <li>
-            <NavLink to='/leader-board' activeClassName='active'>
+            <NavLink to="/leader-board" activeClassName="active">
               Leader Board
             </NavLink>
           </li>
           <li>
-            <NavLink to='/profile' activeClassName='active'>
-              <ProfileWidget />
-            </NavLink>
+            {props.authedUser ? (
+              <NavLink
+                to={`/profile/${props.authedUser}`}
+                activeClassName="active"
+              >
+                <ProfileWidget />
+              </NavLink>
+            ) : (
+              <NavLink to={"/"} activeClassName="inactive">
+                <ProfileWidget />
+              </NavLink>
+            )}
           </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
