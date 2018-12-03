ba0259a2053d8e4722b9bfa55ae01a75745189f9
taehyunlim <taehyun.lim@gmail.com>
2018-12-02 21:14:52 -0800

    Removed unused dependencies

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,74 +1,99 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { ScoreCard } from './ScoreCard' 
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { ScoreCard } from "./ScoreCard";
 
 class LeaderBoard extends Component {
   render() {
-    const { questions, users, authedUser, activeTabSort, selectTabSort } = this.props
+    const { users, authedUser, activeTabSort, selectTabSort } = this.props;
 
     // Sort userIds by sort type
-    let userIdsBySortType = {}
-    const sortTypes = ['totalScore', 'questions', 'answers']
-    
+    let userIdsBySortType = {};
+    const sortTypes = ["totalScore", "questions", "answers"];
+
     sortTypes.forEach(type => {
       switch (type) {
-        case 'questions':
-          userIdsBySortType[type] = Object.keys(users)
-            .sort((a,b) => users[b][type].length - users[a][type].length)
+        case "questions":
+          userIdsBySortType[type] = Object.keys(users).sort(
+            (a, b) => users[b][type].length - users[a][type].length
+          );
           break;
-        case 'answers':
-          userIdsBySortType[type] = Object.keys(users)
-            .sort((a,b) => Object.keys(users[b][type]).length - Object.keys(users[a][type]).length)
+        case "answers":
+          userIdsBySortType[type] = Object.keys(users).sort(
+            (a, b) =>
+              Object.keys(users[b][type]).length -
+              Object.keys(users[a][type]).length
+          );
           break;
-        case 'totalScore':
-          userIdsBySortType[type] = Object.keys(users)
-            .sort((a,b) => getTotalScore(b, users) - getTotalScore(a, users))
+        case "totalScore":
+          userIdsBySortType[type] = Object.keys(users).sort(
+            (a, b) => getTotalScore(b, users) - getTotalScore(a, users)
+          );
           break;
         default:
           break;
       }
-    })
-    
+    });
+
     function getTotalScore(id, users = {}) {
       let countQuesitons = 0,
-      countAnswers = 0
+        countAnswers = 0;
       if (users[id]) {
-        countQuesitons = users[id]['questions'].length
-        countAnswers = Object.keys(users[id]['answers']).length
+        countQuesitons = users[id]["questions"].length;
+        countAnswers = Object.keys(users[id]["answers"]).length;
       }
-      return countQuesitons * 3 + countAnswers
+      return countQuesitons * 3 + countAnswers;
     }
 
     // console.log(userIdsBySortType)
-    const activeSortBy = sortTypes[activeTabSort]
+    const activeSortBy = sortTypes[activeTabSort];
 
     // Only return page if authedUser is set
-    if (!authedUser) return null
+    if (!authedUser) return null;
     return (
-      <div className='page-container'>
-        <div className='tabList'>
-          <button className={`tabBtn ${(activeTabSort===0) && 'activeBtn'}`} onClick={() => {selectTabSort(0)}}>Sort By Total Score</button>
-          <button className={`tabBtn ${(activeTabSort===1) && 'activeBtn'}`} onClick={() => {selectTabSort(1)}}>Sort By Questions</button>
-          <button className={`tabBtn ${(activeTabSort===2) && 'activeBtn'}`} onClick={() => {selectTabSort(2)}}>Sort By Answers</button>
+      <div className="page-container">
+        <div className="tabList">
+          <button
+            className={`tabBtn ${activeTabSort === 0 && "activeBtn"}`}
+            onClick={() => {
+              selectTabSort(0);
+            }}
+          >
+            Sort By Total Score
+          </button>
+          <button
+            className={`tabBtn ${activeTabSort === 1 && "activeBtn"}`}
+            onClick={() => {
+              selectTabSort(1);
+            }}
+          >
+            Sort By Questions
+          </button>
+          <button
+            className={`tabBtn ${activeTabSort === 2 && "activeBtn"}`}
+            onClick={() => {
+              selectTabSort(2);
+            }}
+          >
+            Sort By Answers
+          </button>
         </div>
-        <ul className={'questions-list'}>
-        {userIdsBySortType[activeSortBy].map((userId) => (
-          <li key={userId}>
-            <ScoreCard key={userId} user={users[userId]} />
-          </li>
-        ))}
+        <ul className={"questions-list"}>
+          {userIdsBySortType[activeSortBy].map(userId => (
+            <li key={userId}>
+              <ScoreCard key={userId} user={users[userId]} />
+            </li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps ({ questions, users, authedUser }) {
+function mapStateToProps({ questions, users, authedUser }) {
   return {
-    questions,
     users,
     authedUser
-  }
+  };
 }
 
-export default connect(mapStateToProps)(LeaderBoard)
+export default connect(mapStateToProps)(LeaderBoard);
--- src/components/ScoreCard.js
+++ src/components/ScoreCard.js
@@ -1,35 +1,30 @@
-import React, { Component, Fragment } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const ScoreCard = (props) => {
-  const { user } = props
-  if (!user) return <p>[404] User does not exist.</p>
-  const { id, name, avatarURL, questions, answers } = user
-  const countQ = questions.length
-  const countA = Object.keys(answers).length
+export const ScoreCard = props => {
+  const { user } = props;
+  if (!user) return <p>[404] User does not exist.</p>;
+  const { id, name, avatarURL, questions, answers } = user;
+  const countQ = questions.length;
+  const countA = Object.keys(answers).length;
   return (
-    <div className='card-container'>
+    <div className="card-container">
       <Link to={`/profile/${id}`}>
-        <div className='card-top'>{name}</div>
+        <div className="card-top">{name}</div>
       </Link>
-      <div className='card-main'>
-        <div className='scorecard-content-left'>
-          <img src={avatarURL} alt={`${name}`} className='avatar' />
+      <div className="card-main">
+        <div className="scorecard-content-left">
+          <img src={avatarURL} alt={`${name}`} className="avatar" />
         </div>
-        <div className='scorecard-content-center'>
-          <div className='text'># of Questions: {countQ}</div>
-          <div className='text'># of Answers: {countA}</div>
+        <div className="scorecard-content-center">
+          <div className="text"># of Questions: {countQ}</div>
+          <div className="text"># of Answers: {countA}</div>
         </div>
-        <div className='scorecard-content-right'>
+        <div className="scorecard-content-right">
           <span>Total Score</span>
-          <div className={'score'}>
-            {countA + countQ}
-          </div>
-
+          <div className={"score"}>{countA + countQ}</div>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
